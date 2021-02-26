6e8da178e9b23224386eb93b6bb7fd79d044a5e4
Oisin Brady <oisinbrady@pm.me>
2021-02-26 16:17:22 +0000

    WIP hdbscan w/ 2opt

--- aco_pants.py
+++ aco_pants.py
@@ -7,6 +7,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from itertools import cycle
 import networkx as nx
+import hdbscan
+import seaborn as sns
 
 
 # TODO seperate progam to solve via standalone ACO strategy
@@ -14,22 +16,6 @@ import networkx as nx
 
 # TODO find alternative clustering algorithms
 # 	IDEA: DB clustering, will need to calculate centroids if using current solution
-
-
-def plot_cluster_graph(n_clusters_, cluster_centers, cities_np, labels) -> None:
-	plt.figure(1)
-	plt.clf()
-
-	colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
-	for k, col in zip(range(n_clusters_), colors):
-	    my_members = labels == k
-	    cluster_center = cluster_centers[k]
-	    plt.plot(cities_np[my_members, 0], cities_np[my_members, 1], col + '.')
-	    plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
-	             markeredgecolor='k', markersize=14)
-	plt.title('Estimated number of clusters: %d' % n_clusters_)
-	plt.savefig("graph.png")
-	plt.close()
 
 
 # length function for weight value of edges
@@ -44,10 +30,10 @@ def get_cities() -> list:
 	# Therefore, need to reconsider clustering algorithm
 	# filename = "data_sets/ar9125_nodes.txt"  
 
-	# filename = "data_sets/qa194_output.txt" # Qatar: 119 "cities"
+	filename = "data_sets/qa194_output.txt" # Qatar: 119 "cities"
 	# filename = "gen_cities.txt"  # 116 cities
 	# filename = "city_xy_2"  # 48 cities
-	filename = "city_xy"  # 15 cities
+	# filename = "city_xy"  # 15 cities
 	nodes = []
 
 	with open(filename, 'r') as f:
@@ -78,7 +64,7 @@ def draw_solution(path:list) -> None:
 	plt.savefig("solution.png")
 
 
-def tour_distance(path:list) -> None:
+def tour_distance(path:list) -> int:
 	total_distance = 0
 	for i, n in enumerate(path):
 		if i == len(path) - 1:
@@ -86,6 +72,7 @@ def tour_distance(path:list) -> None:
 		else:
 			total_distance = total_distance + euclidean(n, path[i+1])
 	print(f"total distance of tour = {total_distance}")
+	return total_distance
 
 
 def find_link_nodes(ordered_clusters:list) -> None:
@@ -183,45 +170,164 @@ def rebuild_path(ordered_clusters:list) -> None:
 				ordered_clusters[i][1] = [c[1][s_loc]] + l + r + [c[1][e_loc]]
 
 
+def two_opt_swap(cluster_path:list, start:int, stop:int) -> list:
+	new_route = []
+	for i in range(0, start):
+		new_route.append(cluster_path[i])
+	for i in range(stop, start, -1):
+		new_route.append(cluster_path[i])
+	for i in range(stop, len(cluster_path)):
+		new_route.append(cluster_path[i])
+
+	print(f"new_route: {new_route}\n")
+	return new_route
+
+
+def two_opt(inter_cluster_path:list, cluster_cores:list, clusters:list) -> None:
+	# TODO find nodes that are part of a cluster (core nodes - see cluster's meta info)
+	# Calculate midpoints between core node and its adjacent nodes 
+	# For all nodes within the cluster of this core node, find one node closests to m_1 
+		# and one closests to m_2
+	# perform 2-opt on the clusters' nodes when node_m1 and node_m2 must stay in their
+		# original order
+	# remove the current code node from the ordered_clusters list 
+	# and replace with the 2-opt path
+
+
+
+	# get cluster with core node
+	for c_core in cluster_cores:
+		cluster_label = c_core[1]
+		core_node = c_core[0]
+		c_node_loc = None
+
+		p_node = None  # previous
+		n_node = None  # next
+		# get core node index in inter_cluster_path
+
+		for i, n in enumerate(inter_cluster_path):
+			if n == core_node:
+				c_node_loc = i
+				# get subsequent adjacent nodes
+				if i == 0:
+					p_node = inter_cluster_path[len(inter_cluster_path) - 1]
+				else:
+					p_node = inter_cluster_path[i - 1]
+				if i == len(inter_cluster_path) - 1:
+					n_node = inter_cluster_path[0]
+				else:
+					n_node = inter_cluster_path[i + 1]
+				break
+
+		# calculate m_prev, m_next: midpoints between adjacent nodes and core node
+		m_prev = [(p_node[0] + core_node[0])/2, (p_node[1] + core_node[1])/2]
+		m_next = [(n_node[0] + core_node[0])/2, (n_node[1] + core_node[1])/2]
+
+		start_node = None
+		end_node = None
+		cluster_path = list()
+		for c in clusters:
+			# find cluster
+			print(f" OLD cluster_path: {c[1]}\n")
+			if c[0][0] == cluster_label:
+				print("YES")
+				
+				start_node = sorted(c[1], key=lambda x:euclidean(x,m_prev))[len(c[1])-1]
+				end_node = sorted([n for n in c[1] if n != start_node], key=lambda x:euclidean(x,m_next))[len(c[1])-2]
+				cluster_path.append(start_node)
+				for n in c[1]:
+					if n != start_node and n != end_node:
+						cluster_path.append(n)
+				cluster_path.append(end_node)
+				print(f" NEW cluster_path: {cluster_path}\n")
+				break
+
+		improved = True
+		while improved is True:
+			improved = False
+			best_distance = tour_distance(cluster_path)
+			for i in range(1, len(cluster_path) - 1):
+				for j in range(i+1, len(cluster_path) - 1):
+					new_route = two_opt_swap(cluster_path, i, j)
+					new_distance = tour_distance(new_route)
+					if new_distance < best_distance:
+						cluster_path = new_route
+						best_distance = new_distance
+						improved = True
+
+		print(f"NEW CLUSTER PATH 2 OPT: {cluster_path}\n")
+
+
+		l = inter_cluster_path[0:c_node_loc - 1]
+		mid = cluster_path
+		r = inter_cluster_path[c_node_loc + 1: len(inter_cluster_path) - 1]
+		inter_cluster_path = l + mid + r
+
+	print(len(inter_cluster_path))
+	# 2-opt with m1, ... ,m2 (m1,m2 in fixed pos)
+	# replace core node in inter_cluster_path with 2-opt list
+	
+
+
+
+
+
+
 def main():
 	nodes = get_cities()
 
 	# apply clustering algorithm
 
 	cities_np = np.array(nodes)
-	ms = MeanShift(bin_seeding=True)  # TODO
-	ms.fit(cities_np)  # use mean shift algorithm
-	labels = ms.labels_
-	cluster_centers = ms.cluster_centers_  # centroids of all clusters
+	clusterer = hdbscan.HDBSCAN()
+	clusterer.fit(cities_np)
+	color_palette = sns.color_palette('deep', 1000)
+	cluster_colors = [color_palette[x] if x >= 0 else (0.5, 0.5, 0.5) for x in clusterer.labels_]
+	cluster_member_colors = [sns.desaturate(x, p) for x, p in zip(cluster_colors, clusterer.probabilities_)]
+	# plot as scatter graph
+	plt.scatter(*cities_np.T, s=50, linewidth=0, c=cluster_member_colors, alpha=0.75)
+	plt.savefig("graph_hdbscan.png")
 
-	labels_unique = np.unique(labels)
-	n_clusters_ = len(labels_unique)
-
-	plot_cluster_graph(n_clusters_, cluster_centers, cities_np, labels)
-
+	print(clusterer.labels_)
+	# print(clusterer.probabilities_.tolist())
+	
 	# initialise cluster's list. Holds paths and meta data. I.e. link nodes 
 	clusters = list()
-	for k in range(n_clusters_):
-		meta_info = list()  # to contain centroid xy, and link nodes (2) xy
-		meta_info.append(cluster_centers[k].tolist())  # add centroid value
-		cities = list()  # xy coordinates of all city nodes
-		for i, city_xy in enumerate(cities_np.tolist()):
-			if labels[i] == k:
-				cities.append(city_xy)  # add city belonging to current cluster
-		clusters.append([meta_info, cities])
+	cluster_cores = list()  # pseudo-centroids and the cluster they belong to
 
+	for k in np.unique(clusterer.labels_):
+		meta_info = [k]  # contains: label, 1 core node (pseudo-centroid), link nodes (later)
+		cluster_nodes = []
+		for i, node in enumerate(cities_np.tolist()):
+			if clusterer.labels_[i] == k:
+				cluster_nodes.append(node)
+				if len(meta_info) == 1 and meta_info[0] != -1:
+					if clusterer.probabilities_[i] == 1.0:
+						# add a core node 
+						meta_info.append(node)
+						cluster_cores.append([node, k])
+		clusters.append([meta_info, cluster_nodes])
 
-	# Determine inter-cluster path via ACO - using centroids as artificial nodes
-	world = pants.World(cluster_centers.tolist(), euclidean)
+	inter_cluster_nodes = list()
+	for c in clusters:
+		if c[0][0] == -1:
+			for n in c[1]:
+				inter_cluster_nodes.append(n)  # add all outlier nodes (not part of a cluster)
+		else:
+			inter_cluster_nodes.append(c[0][1])  # add core node
+
+	# Determine inter-cluster path via ACO
+	world = pants.World(inter_cluster_nodes, euclidean)
 	solver = pants.Solver()
-	inter_cluster_solution = solver.solve(world)
+	solution = solver.solve(world)
 
 	# re-order clusters array according to inter_cluster_solution
-	ordered_clusters = list()
-	for c in inter_cluster_solution.tour:
-		for k in clusters:
-			if c == k[0][0]:
-				ordered_clusters.append(k)
+	inter_cluster_path = [n for n in solution.tour]
+	print(cluster_cores)
+	
+	two_opt(inter_cluster_path, cluster_cores, clusters)
+
+	return 0
 
 	# Find mid-point b/w clusters to link nodes b/w clusters 
 	find_link_nodes(ordered_clusters)
